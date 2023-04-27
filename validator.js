9d5489fc9b56107be6a3ce59282284b83fbd4cd2
Nathaly Santos <nathalysantos0103@gmail.com>
2023-04-27 12:34:28 -0500

    Updates

--- validator.js
+++ validator.js
@@ -6,25 +6,27 @@ const validator = {
     const numerosReverse = numerosTarjeta.reverse(); // reverse() con el metodo revertimos el string
     let sumaNumeros = 0; // se inicia en 0 pa ir sumando
     for (let i = 0; i < numerosReverse.length; i++) {
-      let digito = numerosReverse[i];// se recorre numerosReverse y se almacena en digitos
+      let digito = numerosReverse[i]; // se recorre numerosReverse y se almacena en digitos
       if (i % 2 === 1) {
         digito *= 2;
         if (digito > 9) {
           digito -= 9;
         }
-      } 
-      sumaNumeros += digito; //sumaNumeros se le suman los digitos ya que esta vacio arriba 
+      }
+      sumaNumeros += digito; //sumaNumeros se le suman los digitos ya que esta vacio arriba
     }
-  
-    return sumaNumeros % 10 === 0 //si el residuo de suma es 0 ya se coloca en true 
+
+    return sumaNumeros % 10 === 0; //si el residuo de suma es 0 ya se coloca en true
   },
   maskify: function (creditCardNumber) {
     if (creditCardNumber.length <= 4) {
       return creditCardNumber; // Verificar si la cadena es menor o igual a 4 caracteres si es asi solo va a mostrar esos 4 caracteres
     } else {
-      return "#".repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4); //creditCardNumber.slice(-4) pido los ultimos 4 digitos
+      return (
+        "#".repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4)
+      ); //creditCardNumber.slice(-4) pido los ultimos 4 digitos
     }
   },
 };
 
-export default validator;
+export default validator; 
